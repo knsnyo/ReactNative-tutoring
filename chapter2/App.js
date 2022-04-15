5a5621627b8849e2c308e5919f0ceb43bfc63567
knsnyo <isc963@naver.com>
2022-04-15 18:59:38 +0900

    20220415 tutoring

--- chapter2/App.js
+++ chapter2/App.js
@@ -1,15 +1,52 @@
-import { StyleSheet, Text, SafeAreaView } from "react-native";
+import { StyleSheet, Text, SafeAreaView, View, ScrollView } from "react-native";
 
 export default function App() {
-  const todos = ["김장현", "const변수", "JSX 문법", "array", "function"];
-
+  const todos = [
+    "const변수",
+    "JSX 문법",
+    "array",
+    "function",
+    "JSX 문법",
+    "array",
+    "function",
+    "JSX 문법",
+    "array",
+    "function",
+    "JSX 문법",
+    "array",
+    "function",
+    "JSX 문법",
+    "array",
+    "function",
+  ];
+  const student = {
+    name: "김장현",
+    msg: "용서하겠습니다",
+    music: "웬디 - Goodbye",
+  };
   const todoList = todos.map((todo) => {
-    return (<Text style={styles.todo}>{todo}</Text>);
+    return (
+      <View style={styles.todoItem}>
+        <Text>{todo}</Text>
+      </View>
+    );
   });
 
   return (
     <SafeAreaView style={styles.container}>
-      {todoList}
+      <View style={styles.user}>
+        <View style={styles.userPicture}></View>
+        <View style={styles.userInfo}>
+          <Text style={styles.username}>{student.name}</Text>
+          <Text>{student.msg}</Text>
+        </View>
+        <View style={styles.userMusic}>
+          <Text style={styles.melon}>{student.music}</Text>
+        </View>
+      </View>
+      <View style={styles.todoList}>
+        <ScrollView style={{ width: "100%" }}>{todoList}</ScrollView>
+      </View>
     </SafeAreaView>
   );
 }
@@ -20,8 +57,50 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+    flexDirection: "column",
   },
-  todo: {
-    backgroundColor: "#ddd",
-  }
+  user: {
+    flex: 1,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    borderColor: "#ddd",
+    borderBottomWidth: 1,
+  },
+  userPicture: {
+    flex: 1,
+  },
+  userInfo: {
+    flex: 2,
+    justifyContent: "center",
+    alignItems: "flex-start",
+  },
+  username: {
+    fontWeight: "bold",
+  },
+  userMusic: {
+    flex: 2,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  melon: {
+    borderColor: "green",
+    borderWidth: 2,
+    padding: 10,
+    borderRadius: 30,
+  },
+  todoList: {
+    flex: 8,
+    backgroundColor: "#fff",
+    width: "100%",
+    alignItems: "center",
+  },
+  todoItem: {
+    width: "100%",
+    borderColor: "#ddd",
+    borderBottomWidth: 1,
+    height: 50,
+    justifyContent: "center",
+    alignItems: "center",
+  },
 });
